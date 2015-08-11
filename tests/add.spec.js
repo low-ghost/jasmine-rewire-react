55fc0d12f99149c167dff504c9de1b2800027db0
low_ghost <low_ghost@protonmail.com>
2015-08-10 22:51:24 -0400

    babel transform

--- tests/add.spec.js
+++ tests/add.spec.js
@@ -1,5 +1,9 @@
-describe("the jasmine framework", function() {
-    it('should run in the first place', function() {
+import { add } from '../add';
+describe("the jasmine framework", () => {
+    it('should run in the first place', () => {
         expect(1 + 1).toEqual(2);
     });
+    it('should do the add function', () => {
+        expect(add(1, 1)).toEqual(2);
+    })
 });
